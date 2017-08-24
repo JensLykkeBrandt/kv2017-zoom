e49bb69faa08c5de42d4194dc758c1755f067e4d
Johannes Wehner <johw@dr.dk>
2017-08-24 15:32:28 +0200

    a

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,26 +16,15 @@ function init() {
     map = document.getElementById('kv-2017-map');
     document.body.classList.add('kv-2017-map-zoom-' + states[state]);
 
-    console.log(map);
-
-    //const ex = new Example();
     document.addEventListener('click', () => {
-        console.log(map.classList);
         document.body.classList.remove('kv-2017-map-zoom-' + states[state]);
         state = (state === states.length-1) ? 0 : state+1;
         document.body.classList.add('kv-2017-map-zoom-' + states[state]);
-
-        console.log('click', state);
     });
 
 
 }
 
-
-
-function changeState() {
-    console.log();
-}
 
 
 document.addEventListener('DOMContentLoaded', () => {
